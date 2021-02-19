58762cd89a27d7555038852110e106d16136a35d
Ross MacArthur <ross@macarthur.io>
2021-02-19 14:15:01 +0200

    Remove specialize impl for `Styled`
    Instead have dedicated methods on `Styled` for chained updates to the
    styling.

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,40 @@ pub struct Styled<T> {
     style: Style,
 }
 
+macro_rules! meth_color {
+    ($meth:ident, $color:ident) => {
+        pub fn $meth(self) -> Self {
+            self.on(Color::$color)
+        }
+    };
+    ($meth:ident, $color:ident($($arg:ident),+)) => {
+        pub fn $meth(self, $($arg: u8),+) -> Self {
+            self.fg(Color::$color($($arg),+))
+        }
+    };
+}
+
+macro_rules! meth_style {
+    ($meth:ident) => {
+        pub fn $meth(self) -> Self {
+            let Self { inner, style } = self;
+            Self {
+                inner,
+                style: style.$meth(),
+            }
+        }
+    };
+    ($meth:ident, $color:ident) => {
+        pub fn $meth(self, $color: Color) -> Self {
+            let Self { inner, style } = self;
+            Self {
+                inner,
+                style: style.$meth($color),
+            }
+        }
+    };
+}
+
 impl<T> Styled<T> {
     /// Construct a new `Styled`.
     fn new(inner: T) -> Self {
@@ -19,6 +53,30 @@ impl<T> Styled<T> {
             style: Style::new(),
         }
     }
+
+    // different styles
+    meth_style!(bold);
+    meth_style!(dimmed);
+    meth_style!(italic);
+    meth_style!(underline);
+    meth_style!(blink);
+    meth_style!(reverse);
+    meth_style!(hidden);
+    meth_style!(strikethrough);
+    meth_style!(fg, color);
+    meth_style!(on, color);
+
+    // different colors
+    meth_color!(black, Black);
+    meth_color!(red, Red);
+    meth_color!(green, Green);
+    meth_color!(yellow, Yellow);
+    meth_color!(blue, Blue);
+    meth_color!(magenta, Purple);
+    meth_color!(cyan, Cyan);
+    meth_color!(white, White);
+    meth_color!(fixed, Fixed(n));
+    meth_color!(rgb, RGB(r, g, b));
 }
 
 impl<T> Deref for Styled<T> {
@@ -46,12 +104,12 @@ impl<T: Display> Display for Styled<T> {
 
 macro_rules! meth_color {
     ($meth:ident, $color:ident) => {
-        fn $meth(self) -> Styled<Self::Output> {
+        fn $meth(self) -> Styled<Self> {
             self.on(Color::$color)
         }
     };
     ($meth:ident, $color:ident($($arg:ident),+)) => {
-        fn $meth(self, $($arg: u8),+) -> Styled<Self::Output> {
+        fn $meth(self, $($arg: u8),+) -> Styled<Self> {
             self.fg(Color::$color($($arg),+))
         }
     };
@@ -59,31 +117,19 @@ macro_rules! meth_color {
 
 macro_rules! meth_style {
     ($meth:ident) => {
-        fn $meth(self) -> Styled<Self::Output> {
-            let Styled { inner, style } = self.styled();
-            Styled {
-                inner,
-                style: style.$meth(),
-            }
+        fn $meth(self) -> Styled<Self> {
+            Styled::new(self).$meth()
         }
     };
     ($meth:ident, $color:ident) => {
-        fn $meth(self, $color: Color) -> Styled<Self::Output> {
-            let Styled { inner, style } = self.styled();
-            Styled {
-                inner,
-                style: style.$meth($color),
-            }
+        fn $meth(self, $color: Color) -> Styled<Self> {
+            Styled::new(self).$meth($color)
         }
     };
 }
 
-/// Allows anything implementing `Display` to be styled.
+/// Allows anything implementing [`Display`] to be styled.
 pub trait Stylize<T>: Sized {
-    type Output;
-
-    fn styled(self) -> Styled<Self::Output>;
-
     // different styles
     meth_style!(bold);
     meth_style!(dimmed);
@@ -109,26 +155,5 @@ pub trait Stylize<T>: Sized {
     meth_color!(rgb, RGB(r, g, b));
 }
 
-/// Special implementation for [`Styled`] to prevent unnecessary copying.
-impl<T> Stylize<T> for Styled<T>
-where
-    T: Display,
-{
-    type Output = T;
-
-    fn styled(self) -> Styled<Self::Output> {
-        self
-    }
-}
-
 /// Blanket implementation for everything that implements [`Display`].
-impl<T> Stylize<T> for T
-where
-    T: Display,
-{
-    type Output = Self;
-
-    fn styled(self) -> Styled<Self::Output> {
-        Styled::new(self)
-    }
-}
+impl<T> Stylize<T> for T where T: Display {}
